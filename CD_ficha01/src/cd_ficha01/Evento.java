b0bd6c79f65f2c3c8ca88cc3a635f3a9fa0bfd54
Tiago Matos <aluno23026@ipt.pt>
2024-09-24 12:10:35 +0100

    Criação classe evento

---
+++ CD_ficha01/src/cd_ficha01/Evento.java
@@ -0,0 +1,15 @@
+package cd_ficha01;
+
+
+public class Evento {
+
+    public Evento(String nomePessoa, String nomeEvento, String dataEvento) {
+        this.nomePessoa = nomePessoa;
+        this.nomeEvento = nomeEvento;
+        this.dataEvento = dataEvento;
+    }
+    
+    private String nomePessoa;
+    private String nomeEvento;
+    private String dataEvento;
+}
