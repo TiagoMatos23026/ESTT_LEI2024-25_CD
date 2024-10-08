942e92b80180125983c8b3cdad29e7c59446c417
Tiago Matos <aluno23026@ipt.pt>
2024-10-08 12:01:35 +0100

    .

--- CD_ProjCurriculumDigital/src/CD_Ficha03/Criptografia_Multipla_Assimetrica.java
+++ CD_ProjCurriculumDigital/src/CD_Ficha03/Criptografia_Multipla_Assimetrica.java
@@ -4,10 +4,26 @@
  */
 package CD_Ficha03;
 
+import CD_Ficha03.utils.SecurityUtils;
+import java.security.KeyPair;
+import java.util.Base64;
+
 /**
  *
  * @author asus
  */
 public class Criptografia_Multipla_Assimetrica {
-    
+
+    public static void main(String[] args) throws Exception {
+        String fileName = "image.jpg";
+        //gerar um par de chaves
+        KeyPair kp = SecurityUtils.generateRSAKeyPair(2048);
+        //guardar as chaves
+        SecurityUtils.saveKey(kp, fileName);
+        String message = "hello secret world of RSA";
+        byte[] secret = SecurityUtils.encrypt(message.getBytes(), kp.getPrivate());
+        System.out.println("SECRET : " + Base64.getEncoder().encodeToString(secret));
+        byte[] plain = SecurityUtils.decrypt(secret, kp.getPublic());
+        System.out.println("Mensagem " + new String(plain));
+    }
 }
